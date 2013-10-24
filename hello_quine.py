847f065b6827a474264204339e10a4b4f588e064
BoppreH <boppreh@gmail.com>
2013-10-24 09:35:50 -0200

    Use base64 to minimize quine size

--- hello_quine.py
+++ hello_quine.py
@@ -1,10 +1,6 @@
+import base64
 q = '"""'
-tmp_source = """q = '{}'
-tmp_source = {}
-globals()['__source__'] = tmp_source.format(q, q + tmp_source + q)
+b64source = """aW1wb3J0IGJhc2U2NApxID0gJ3t9JwpiNjRzb3VyY2UgPSB7fQpfX3NvdXJjZV9fID0gYmFzZTY0LmI2NGRlY29kZShiNjRzb3VyY2UpLmZvcm1hdChxLCBxICsgYjY0c291cmNlICsgcSkKCnByaW50IF9fc291cmNlX18gKyAiIiIgIiIiCg=="""
+__source__ = base64.b64decode(b64source).format(q, q + b64source + q)
 
-print __source__
-"""
-globals()['__source__'] = tmp_source.format(q, q + tmp_source + q)
-
-print __source__
+print __source__ + """ """
--- quine.py
+++ quine.py
@@ -1,14 +1,22 @@
+from base64 import b64encode
+
 def quinefy(old_filename, new_filename):
     with open(old_filename) as old_file:
         source = old_file.read()
+        b64source = b64encode(source)
+
     q = '"""'
     b = '{}'
-    tmp_source = """q = '{}'
-tmp_source = {}
-globals()['__source__'] = tmp_source.format(q, q + tmp_source + q)
+    tmp_source = """import base64
+q = '{}'
+b64source = {}
+__source__ = base64.b64decode(b64source).format(q, q + b64source + q)
 
 {}"""
-    quine = tmp_source.format(q, q + tmp_source.format(b, b, source) + q, source)
+    inner = tmp_source.format(b, b, source)
+    b64inner = b64encode(inner)
+    quine = tmp_source.format(q, q + b64inner + q, source)
+
     with open(new_filename, 'w') as new_file:
         new_file.write(quine)
 
