0bb2e30a32ae91129ec28848102a96e5cac50cd8
BoppreH <boppreh@gmail.com>
2014-04-27 22:39:49 -0300

    Update package structure

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name='Quine',
-    version='0.1.0',
+    version=open('CHANGES.txt').read().split()[0],
     author='Lucas Boppre Niehues',
     author_email='lucasboppre@gmail.com',
     packages=['quine'],
     url='http://pypi.python.org/pypi/Quine/',
     license='LICENSE.txt',
     description='Quine generator.',
-    long_description=open('README').read(),
+    long_description=open('README.rst').read() + '\n\Last Updates\n-------------\n' + open('CHANGES.txt').read(),
 )
